2beb7ed4ac9b091ea81cecbf956e8d7f1021f0a5
Aekkarat Taesoongnoen <aekkarat.t@ghb.co.th>
2023-11-15 10:36:35 +0700

    OK

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from  joblib import load
+from joblib import load
 
 
 # st.title('Hello world')
@@ -9,17 +9,17 @@ loaded = load("titanic_model.joblib")
 st.title('Titanic Survival Predictions')
 st.sidebar.title('Menu')
 menu = ['Home', 'Predictions']
-st.sidebar.selectbox('',menu)
+st.sidebar.selectbox('', menu)
 
-age = st.slider('Age',0,100,30)
-sibsp = st.slider('Sibsp',0,8,0)
-parch = st.slider('Parch',0,6,0)
-fare = st.slider('Fare',0.0,512.3292,7.8292)
+age = st.slider('Age', 0, 100, 30)
+sibsp = st.slider('Sibsp', 0, 8, 0)
+parch = st.slider('Parch', 0, 6, 0)
+fare = st.slider('Fare', 0.0, 512.3292, 7.8292)
 
 btn = st.button('Submit')
 
 if btn:
-    input_data = [[age, sibsp, parch,fare]]
+    input_data = [[age, sibsp, parch, fare]]
 
     prediction = loaded.predict(input_data)
 
